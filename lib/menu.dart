1a3d0d2a438dc776f430d8e78a1aebfee9a28d6c
Sherri Boley <swboley@gmail.com>
2026-03-11 11:22:40 -0400

    Disabled menu images by renaming them and disabling them in menu.dart file. They are commented out in menu.dart. these will need to be re-enabled and new photos of the menu added.

--- lib/menu.dart
+++ lib/menu.dart
@@ -5,14 +5,14 @@ class MenuScreen extends StatelessWidget {
   const MenuScreen({Key? key}) : super(key: key);
 
   // Replace with your actual menu images in assets/images/menu/
-  final List<String> menuImages = const [
-    'images/designer-30-.png',
-    'images/designer-31-.png',
-    'images/designer-32-.png',
-    'images/designer-34-.png',
-    'images/designer-35-.png',
-    'images/designer-36-.png',
-  ];
+  // final List<String> menuImages = const [
+  //   'images/designer-30-X.png',
+  //   'images/designer-31-X.png',
+  //   'images/designer-32-X.png',
+  //   'images/designer-34-X.png',
+  //   'images/designer-35-X.png',
+  //   'images/designer-36-X.png',
+  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,28 +23,28 @@ class MenuScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
-          itemCount: menuImages.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, // 2 items per row
-            crossAxisSpacing: 16,
-            mainAxisSpacing: 16,
-            childAspectRatio: 3 / 4, // adjust as needed
-          ),
-          itemBuilder: (context, index) {
-            final imagePath = menuImages[index];
-            return Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              clipBehavior: Clip.antiAlias,
-              child: Image.asset(
-                imagePath,
-                fit: BoxFit.cover,
-              ),
-            );
-          },
-        ),
+        // child: GridView.builder(
+        //   itemCount: menuImages.length,
+        //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        //     crossAxisCount: 2, // 2 items per row
+        //     crossAxisSpacing: 16,
+        //     mainAxisSpacing: 16,
+        //     childAspectRatio: 3 / 4, // adjust as needed
+        //   ),
+        //   itemBuilder: (context, index) {
+        //     final imagePath = menuImages[index];
+        //     return Card(
+        //       shape: RoundedRectangleBorder(
+        //         borderRadius: BorderRadius.circular(16),
+        //       ),
+        //       clipBehavior: Clip.antiAlias,
+        //       child: Image.asset(
+        //         imagePath,
+        //         fit: BoxFit.cover,
+        //       ),
+        //     );
+        //   },
+        // ),
       ),
     );
   }
