01e9bd114765b417a3b1ab566ac244c7fb74d865
Sherri Boley <swboley@gmail.com>
2026-03-11 14:31:58 -0400

    Removing hours in public.main.dart.js

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "609605030"
+    serviceWorkerVersion: "3824520590"
   }
 });
