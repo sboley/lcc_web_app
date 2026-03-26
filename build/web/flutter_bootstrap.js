2a28cff1200d1dda7cb86df1a5b1a91f10dbe247
Sherri Boley <swboley@gmail.com>
2026-03-26 10:45:44 -0400

    Updated today's flavor.

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1838736316"
+    serviceWorkerVersion: "3645247348"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a7725eb7c6cc4f280c6af934d68b1d2c",
+const RESOURCES = {"flutter_bootstrap.js": "37881fe5d483eb963b28d8bed374d55d",
 "version.json": "cd250c6fd73a4765100f9c699d159268",
 "index.html": "5099207ab9ac33d08d73ba29ac70bd15",
 "/": "5099207ab9ac33d08d73ba29ac70bd15",
 "lcc-copy.png": "58d9f74092f3b1b6f26068d2e407c6b8",
-"main.dart.js": "ab333bad5516145f48b691e8bfa3cc3c",
+"main.dart.js": "83a1ee1dabcc7a4138a3cf9da01850a1",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "8cec11d98a51eba2cc9497e6c10853be",
 "icons/lcc-copy.png": "58d9f74092f3b1b6f26068d2e407c6b8",
