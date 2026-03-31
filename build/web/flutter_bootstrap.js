ee383b6985b221c1c360b7bb0919be8238a6c068
Sherri Boley <swboley@gmail.com>
2026-03-31 15:48:57 -0400

    Updated today's flavor.

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "648608423" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4179458683" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
--- build/web/main.dart.js
+++ build/web/main.dart.js
@@ -62716,7 +62716,7 @@ if(a)c.y=A.t(t.N,t.UX)
 s=t.jl
 r=A.d([],s)
 q=c.y.i(0,b)
-if(q==null)q=B.eJ
+if(q==null)q=B.eI
 p=A.t(t.ob,t.UX)
 o=c.y.gbm()
 n=o.eK(o)
@@ -62735,8 +62735,8 @@ if(j){r=A.d([],s)
 i=c.y
 i.toString
 q=i.i(0,h.gdd())
-if(q==null)q=B.eJ}else{r=B.eJ
-q=B.eJ}l=h
+if(q==null)q=B.eI}else{r=B.eI
+q=B.eI}l=h
 continue}if(j){i=h.b
 i=i==null?b:i.gPj()
 j=i===!0}else j=!1
@@ -69957,10 +69957,10 @@ l=e
 k=J.QO(l,new A.UT())
 j=J.QO(l,new A.UU())
 i=B.lv
-h=B.eI
+h=B.eJ
 if(!J.c(k,-1)&&!J.c(j,-1)&&k<j)i=J.ajE(l,k+1,j)
 if(!J.c(j,-1))h=J.ajD(l,j+1)
-g=n.on(J.ck(h)!==0?h:B.eI)
+g=n.on(J.ck(h)!==0?h:B.eJ)
 f=A.aI(["flavors",h,"daily",g,"hours",i],t.N,t.z)
 q=f
 s=1
@@ -69973,7 +69973,7 @@ s=6
 break
 case 3:s=2
 break
-case 6:q=A.aI(["flavors",B.eI,"daily",n.on(B.eI),"hours",B.lv],t.N,t.z)
+case 6:q=A.aI(["flavors",B.eJ,"daily",n.on(B.eJ),"hours",B.lv],t.N,t.z)
 s=1
 break
 case 1:return A.O(q,r)
@@ -74854,7 +74854,6 @@ B.DY=s([B.al,B.n],A.ag("z<xC>"))
 B.S=new A.xG(0,"rtl")
 B.H=new A.xG(1,"ltr")
 B.ih=s([B.S,B.H],A.ag("z<xG>"))
-B.eI=s(["Banana Cheesecake","Toffee Coffee","Chocolate Chip","Peanut Butter & Strawberry Jam","Strawberry Peach","Keylime Pie","Nerds","* Dairy Free: Sunset Sorbet ","* Dairy Free: Grape Sorbet","Salty Caramel","Cookie Dough","Buckeye","Chocolate","Birthday Cake","Dulce De Leche","Peanut Butter Cup","Strawberry","Butter Pecan","Cookie Monster","Blue Moon","Mint Freckle","Vanilla","Cookies-n-creme"],t.s)
 B.fD=new A.hu(0,"leading")
 B.bn=new A.hu(1,"title")
 B.dT=new A.hu(2,"subtitle")
@@ -74889,7 +74888,7 @@ B.Ej=s([],t.E)
 B.Ek=s([],t.fJ)
 B.Ei=s([],t.ER)
 B.Eh=s([],t.tc)
-B.eJ=s([],t.jl)
+B.eI=s([],t.jl)
 B.Ep=s([],t.wi)
 B.Eo=s([],A.ag("z<am_<@>>"))
 B.ij=s([],t.AO)
@@ -74903,6 +74902,7 @@ B.Eg=s([],t.t)
 B.lz=s([],t.ee)
 B.Eq=s([],t.XS)
 B.lA=s(["Monday: 2pm - 8pm","Tuesday: 2pm - 8pm","Wednesday: 2pm - 8pm","Thursday: 2pm - 8pm","Friday: 12pm- 9pm","Saturday: 12pm - 9pm","Sunday: 12pm - 8pm"],t.s)
+B.eJ=s(["New York Cheesecake","Toffee Coffee","Chocolate Chip","Peanut Butter & Strawberry Jelly","Strawberry Peach","Keylime Pie","Nerds","* Dairy Free: Sunset Sorbet ","* Dairy Free: Strawberry Sorbet","Salty Caramel","Cookie Dough","Buckeye","Chocolate","Birthday Cake","Dulce De Leche","Peanut Butter Cup","Strawberry","Butter Pecan","Cookie Monster","Blue Moon","Mint Freckle","Vanilla","Cookies-n-creme"],t.s)
 B.dd=s([B.bG,B.bo,B.e_,B.e0,B.h1],t.QP)
 B.Dz=s([0.001200833568784504,0.002389694492170889,0.0002795742885861124],t.n)
 B.Ee=s([0.0005891086651375999,0.0029785502573438758,0.0003270666104008398],t.n)
