08ce44d4c54cefa0ce4a07b3c1ef62e40fcb5435
Sherri Boley <swboley@gmail.com>
2026-04-29 10:32:48 -0400

    Updated today's flavors

--- lib/ice-cream-flavors.dart
+++ lib/ice-cream-flavors.dart
@@ -18,7 +18,7 @@ class FlavorService {
 
   Future<Map<String, dynamic>> getFlavorData() async {
     const defaultFlavors = [
-      "Fairy Dream",
+      "M&M Swirl",
       "Peanut Butter Fudge Brownie",
       "Strawberry Cheesecake",
       "Birthday Cake Cookie Dough",
@@ -27,7 +27,7 @@ class FlavorService {
       "Sweet Caramel Coconut",
           "* Cherry Sorbet",
           "* Green Berry Sorbet",
-      "SOLD OUT! Salty Caramel",
+      "Salty Caramel",
       "Cookie Dough",
       "Buckeye",
       "Chocolate",
@@ -209,30 +209,30 @@ class _FlavorScreenState extends State<FlavorScreen> {
                       final flavor = flavors[index];
                       final isDaily = flavor == daily;
 
-                      if (flavor.contains("SOLD OUT!") && flavor.contains("Salty Caramel")) {
-                        return Card(
-                          color: isDaily ? Colors.pink[100] : Colors.white,
-                          child: ListTile(
-                            leading: const Icon(Icons.icecream, color: Colors.brown),
-                            title: Text.rich(
-                              TextSpan(
-                                children: [
-                                  const TextSpan(text: "SOLD OUT! "),
-                                  const TextSpan(
-                                    text: "Salty Caramel",
-                                    style: TextStyle(
-                                      decoration: TextDecoration.lineThrough,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            trailing: isDaily
-                                ? const Icon(Icons.check_circle, color: Colors.pink)
-                                : null,
-                          ),
-                        );
-                      }
+                      // if (flavor.contains("SOLD OUT!") && flavor.contains("Salty Caramel")) {
+                      //   return Card(
+                      //     color: isDaily ? Colors.pink[100] : Colors.white,
+                      //     child: ListTile(
+                      //       leading: const Icon(Icons.icecream, color: Colors.brown),
+                      //       title: Text.rich(
+                      //         TextSpan(
+                      //           children: [
+                      //             const TextSpan(text: "SOLD OUT! "),
+                      //             const TextSpan(
+                      //               text: "Salty Caramel",
+                      //               style: TextStyle(
+                      //                 decoration: TextDecoration.lineThrough,
+                      //               ),
+                      //             ),
+                      //           ],
+                      //         ),
+                      //       ),
+                      //       trailing: isDaily
+                      //           ? const Icon(Icons.check_circle, color: Colors.pink)
+                      //           : null,
+                      //     ),
+                      //   );
+                      // }
 
                       return Card(
                         color: isDaily ? Colors.pink[100] : Colors.white,
